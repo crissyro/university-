c3e3b7f176a29ff75de60aef4edb1cb3526ca57b
Roman Moroz <morozdef24@gmail.com>
2024-03-03 19:32:47 +0300

    Revert "create some functions"
    This reverts commit e0c75d39068577ede6e674e0acbcbb0e2ea27e29.

--- data_structures/matrix/matrix.c
+++ data_structures/matrix/matrix.c
@@ -1,26 +1,6 @@
 #include "matrix.h"
 
 
-void insertionSort(int *a, const size_t size) {
-    for (size_t i = 1; i < size; i++) {
-        int t = a[i];
-        int j = i;
-        while (j > 0 && a[j - 1] > t) {
-            a[j] = a[j - 1];
-            j--;
-        }
-        a[j] = t;
-    }
-}
-
-
-void check_Memory_(int *a) {
-    if (*a == NULL) {
-        fprintf(stderr, "Memory allocation failed\n");
-        exit(1);
-    }
-}
-
 //размещает в динамической памяти матрицу размером nRows на nCols
 matrix getMemMatrix(int nRows, int nCols) {
     int **values = (int **) malloc(sizeof(int*) * nRows);
@@ -70,66 +50,30 @@ void inputMatrices(matrix *ms, int nMatrices) {
 
 //вывод матрицы m
 void outputMatrix(matrix m) {
-    for (size_t i = 0; i < m.nRows; i++) {
-        printf("/n");
+    for (size_t i = 0; i < m.nRows; i++)
         for (size_t j = 0; j < m.nCols; j++)
             printf("%d", m.values[i][j]);
-    }
-    printf("/n");
 }
 
 
 //вывод массива из nMatrices матриц, хранящейся по адресу ms
 void outputMatrices(matrix *ms, int nMatrices) {
-    for (size_t i = 0; i < nMatrices; i++) {
+    for (size_t i = 0; i < nMatrices; i++)
         outputMatrix(ms[i]);
-    }
 }
 
 
 //обмен строк с порядковыми номерами i1 и i2 в матрице m.
-void swapRows(matrix m, int i1, int i2) {
-    assert(i1 >= 0 && i1 < m.nRows && i2 >= 0 && i2 < m.nRows);
-
-    int *temp = m.values[i1];
-    m.values[i1] = m.values[i2];
-    m.values[i2] = temp;
-}
+void swapRows(matrix m, int i1, int i2) ;
 
 
 //обмен колонок с порядковыми номерами j1 и j2 в матрице m
-void swapColumns(matrix m, int j1, int j2) {
-    assert(j1 >= 0 && j1 < m.nCols && j2 >= 0 && j2 < m.nCols);
+void swapColumns(matrix m, int j1, int j2) ;
 
-    for (int i = 0; i < m.nRows; i++) {
-        int temp = m.values[i][j1];
-        m.values[i][j1] = m.values[i][j2];
-        m.values[i][j2] = temp;
-    }
-}
 
 //выполняет сортировку вставками строк матрицы m
 //по неубыванию значения функции criteria применяемой для строк
-void insertionSortRowsMatrixByRowCriteria(matrix m, int (*criteria)(int*, int)) {
-    int *array_criteria = malloc(m.nRows * sizeof(int));
-    check_Memory_(array_criteria);
-    for (int i = 0; i < m.nRows; i++) {
-        array_criteria[i] = criteria(m.values[i], m.nCols);
-    }
-    for (int i = 1; i < m.nRows; i++) {
-        int key = array_criteria[i];
-        int *key_row = m.values[i];
-        int j = i - 1;
-        while (j >= 0 && array_criteria[j] > key) {
-            array_criteria[j + 1] = array_criteria[j];
-            swapRows(m, j + 1, j);
-            j = j - 1;
-        }
-        array_criteria[j + 1] = key;
-        m.values[j + 1] = key_row;
-    }
-    free(array_criteria);
-}
+void insertionSortRowsMatrixByRowCriteria(matrix m, int (*criteria)(int*, int)) ;
 
 
 //выполняет сортировку выбором столбцов матрицы m
@@ -139,38 +83,17 @@ void selectionSortColsMatrixByColCriteria(matrix m, int (*criteria)(int*, int)) 
 
 //возвращает значение ’истина’, если
 //матрица m является квадратной, ложь – в противном случае
-bool isSquareMatrix(matrix *m) {
-    return m->nRows == m->nCols;
-}
+bool isSquareMatrix(matrix *m) ;
 
 
 //возвращает значение ’истина’,
 //если матрицы m1 и m2 равны, ложь – в противном случае
-bool areTwoMatricesEqual(matrix *m1, matrix *m2) {
-    if (m1->nRows != m2->nRows || m1->nCols != m2->nCols)
-        return false;
-    for (int i = 0; i < m1->nRows; i++)
-        for (int j = 0; j < m1->nCols; j++) {
-            if (m1->values[i][j] != m2->values[i][j])
-                return false;
-        }
-
-    return true;
-}
+bool areTwoMatricesEqual(matrix *m1, matrix *m2) ;
 
 
 // возвращает значение ’истина’, если матрица
 //m является единичной, ложь – в противном случае
-bool isEMatrix(matrix *m) {
-    if (!isSquareMatrix(m))
-        return false;
-
-    for(size_t i = 0; i < m->nRows; i++) {
-        if (m->values[i][i] != 1)
-            return false;
-    }
-    return true;
-}
+bool isEMatrix(matrix *m) ;
 
 
 //возвращает значение ’истина’,
--- data_structures/matrix/matrix.h
+++ data_structures/matrix/matrix.h
@@ -4,7 +4,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
-#include <assert.h>
 
 typedef struct matrix {
     int **values; // элементы матрицы
