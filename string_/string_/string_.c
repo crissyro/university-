b986d7e1bb200b86f07e73f8138de74ef833b205
Roman Moroz <morozdef24@gmail.com>
2024-03-17 16:20:13 +0300

    create find functions

--- string_/string_/string_.c
+++ string_/string_/string_.c
@@ -24,4 +24,37 @@ size_t strlen_(const char *begin) {
     return end - begin;
 }
 
+char *find(char *begin, char *end, int ch) {
+    while (begin != end && *begin != ch)
+        begin++;
+    return begin;
+}
+
+char *findNonSpace(char *begin) {
+    while (isspace(*begin) && *begin != '\0') {
+        begin++;
+    }
+    return begin;
+}
+
+char *findSpace(char *begin) {
+    while (!isspace(*begin) && *begin != '\0') {
+        begin++;
+    }
+    return begin;
+}
+
+char *findNonSpaceReverse(char *rbegin, const char *rend) {
+    while (isspace(*rbegin) && *rbegin != *rend) {
+        rbegin--;
+    }
+    return rbegin;
+}
+
+char *findSpaceReverse(char *rbegin, const char *rend) {
+    while (!isspace(*rbegin) && *rbegin != *rend) {
+        rbegin--;
+    }
+    return rbegin;
+}
 
