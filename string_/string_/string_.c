c5355a4b56ffb6faa3546b5eb60fb382dcaf61e4
Roman Moroz <morozdef24@gmail.com>
2024-03-17 15:47:58 +0300

    create len functions

--- string_/string_/string_.c
+++ string_/string_/string_.c
@@ -1,2 +1,27 @@
 #include "string_.h"
 
+
+size_t strlen_v1(char *s) {
+    int i = 0;
+    while (s[i] != '\0')
+        i++;
+    return i;
+}
+
+size_t strlen_v2(char *s) {
+    int i = 0;
+    while (*s != '\0') {
+        i++;
+        s++;
+    }
+    return i;
+}
+
+size_t strlen_(const char *begin) {
+    char *end = begin;
+    while (*end != '\0')
+        end++;
+    return end - begin;
+}
+
+
