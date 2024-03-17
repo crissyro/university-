aaf0cae05a92130b0c332ad07d2988a699aa880f
Roman Moroz <morozdef24@gmail.com>
2024-03-17 16:38:43 +0300

    create memory func/tests

--- tests/string_tests.c
+++ tests/string_tests.c
@@ -1,29 +1,3 @@
-#include "string_.h"
-
-
-size_t strlen_v1(char *s) {
-    int i = 0;
-    while (s[i] != '\0')
-        i++;
-    return i;
-}
-
-size_t strlen_v2(char *s) {
-    int i = 0;
-    while (*s != '\0') {
-        i++;
-        s++;
-    }
-    return i;
-}
-
-size_t strlen_(const char *begin) {
-    char *end = begin;
-    while (*end != '\0')
-        end++;
-    return end - begin;
-}
-
 char *find(char *begin, char *end, int ch) {
     while (begin != end && *begin != ch)
         begin++;
@@ -97,4 +71,3 @@ char* copyIfReverse(char *rbeginSource, const char *rendSource, char *beginDesti
 
     return beginDestination;
 }
-
