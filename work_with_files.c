c510829b55ec7f516454c78e215c85aafd847c6e
Roman Moroz <morozdef24@gmail.com>
2024-04-07 17:17:32 +0300

    make functions for task 7

--- work_with_files.c
+++ work_with_files.c
@@ -8,9 +8,19 @@
 #include <time.h>
 #include <math.h>
 
+void check_Correct_Open_File(FILE *f) {
+    if (f == NULL) {
+        printf("errno %d\n", errno);
+        perror("f");
+        exit(1);
+    }
+}
+
+
 void WriteMatricesBinaryFile(char *file_name, FILE *file, matrix *data,int count_matrices) {
     char *c;
     file = fopen(file_name, "wb");
+    check_Correct_Open_File(file);
 
     c = (char *) data;
     for (int i = 0; i < sizeof(matrix) * count_matrices; i++)
@@ -27,6 +37,8 @@ void SwapMatricesColsAndRowsFile(char *file_name, FILE *file, int count_matrices
     int size = sizeof(matrix) * count_matrices;
     matrix *ptr = malloc(size);
     file = fopen(file_name, "rb");
+    check_Correct_Open_File(file);
+
     c = (char *) ptr;
 
     while ((counter = getc(file)) != EOF) {
@@ -52,6 +64,7 @@ void OutputMatricesInFile(char *file_name, FILE *file, int count_matrices) {
     int size = sizeof(matrix) * count_matrices;
     matrix *ptr = malloc(size);
     file = fopen(file_name, "rb");
+    check_Correct_Open_File(file);
     c = (char *) ptr;
 
     while ((counter = getc(file)) != EOF) {
@@ -62,15 +75,6 @@ void OutputMatricesInFile(char *file_name, FILE *file, int count_matrices) {
     outputMatrices(ptr, count_matrices);
     fclose(file);
     free(ptr);
-}
-
-
-void check_Correct_Open_File(FILE *f) {
-    if (f == NULL) {
-        printf("errno %d\n", errno);
-        perror("f");
-        exit(1);
-    }
 }
 
 char *getWayByTasks(char *filename) {
@@ -208,6 +212,7 @@ void task3() {
 
     char *way_input = getWayByTasks("task3.txt");
     FILE *file = fopen(way_input, "w");
+    check_Correct_Open_File(file);
     WriteGenerateExpression(file);
     fclose(file);
 
@@ -247,9 +252,11 @@ void generateRandomWords(const char* filename) {
 void printCorrectWords(char* inputFilename, char* outputFilename, char* sequence) {
     char* way_input = getWayByTasks(inputFilename);
     FILE* file_input = fopen(way_input, "r");
+    check_Correct_Open_File(file_input);
 
     char* way_output = getWayByTasks(outputFilename);
     FILE* file_output = fopen(way_output, "w");
+    check_Correct_Open_File(file_output);
 
     char word[MAX_N_WORDS_IN_STRING];
 
@@ -270,6 +277,7 @@ void task4() {
 void generateRandomStrings(char* filename) {
     char* way = getWayByTasks(filename);
     FILE* file = fopen(way, "w");
+    check_Correct_Open_File(file);
 
     char alphabet[] = " abcdefg hijklm nopqrst uvwxyz ";
     int alphabet_length = strlen(alphabet);
@@ -294,9 +302,11 @@ void generateRandomStrings(char* filename) {
 void printMaxLenWordsInString(char* inputFilename, char* outputFilename) {
     char* way_input = getWayByTasks(inputFilename);
     FILE* file_input = fopen(way_input, "r");
+    check_Correct_Open_File(file_input);
 
     char* way_output = getWayByTasks(outputFilename);
     FILE* file_output = fopen(way_output, "w");
+    check_Correct_Open_File(file_output);
 
     char s[50];
     while (fgets(s, 50, file_input)) {
@@ -363,6 +373,7 @@ void generatePolinomialArray(char *filename, int nPalinomial, int nMember) {
 
     char *way = getWayByTasks(filename);
     FILE *file = fopen(way, "wb");
+    check_Correct_Open_File(file);
 
     char *c = (char *) array;
     for (int i = 0; i < sizeof(Polinomial) * nPalinomial * nMember * 2; i++) {
@@ -380,6 +391,7 @@ void printCorrectPalinomial(char *filename, int x, int nPalinomial, int nMember)
     Polinomial **array = getMemPalinomeKiller(nPalinomial, nMember);
     char *way = getWayByTasks(filename);
     FILE *file = fopen(way, "rb");
+    check_Correct_Open_File(file);
     c = (char *) array;
 
     while ((counter = getc(file)) != EOF) {
@@ -403,6 +415,7 @@ void printCorrectPalinomial(char *filename, int x, int nPalinomial, int nMember)
     }
 
     file = fopen(way, "wb");
+    check_Correct_Open_File(file);
     c = (char *) array;
     for (int i = 0; i < sizeof(Polinomial) * nPalinomial * nMember * 2; i++) {
         putc(*c++, file);
@@ -418,6 +431,7 @@ void printPalinomialWithFile(char *filename, int nPalinomial, int nMember) {
     Polinomial **array = getMemPalinomeKiller(nPalinomial, nMember);
     char *way = getWayByTasks(filename);
     FILE *file = fopen(way, "rb");
+    check_Correct_Open_File(file);
     c = (char *) array;
 
     while ((counter = getc(file)) != EOF) {
@@ -450,6 +464,157 @@ void task6() {
 
 }
 
+void generateArray(int* a, int size) {
+    srand(time(NULL));
+
+    int half = size/2;
+    int counter_positive = 0, counter_negative = 0, index = 0;
+
+    while (counter_negative < half && counter_positive < half) {
+        int random_num = generateRandomSign() * (rand() % 100 + 1);
+        a[index++] = random_num;
+
+        if (random_num > 0) {
+            counter_positive++;
+        } else {
+            counter_negative++;
+        }
+    }
+
+    while (counter_negative < half) {
+        int random_num = -1 * (rand() % 100 + 1);
+        a[index++] = random_num;
+        counter_negative++;
+    }
+
+    while (counter_positive < half) {
+        int random_num = rand() % 100 + 1;
+        a[index++] = random_num;
+        counter_positive++;
+    }
+}
+
+void generateArrayBinaryFile(char *filename,int *a, int size) {
+    char *way = getWayByTasks(filename);
+    FILE* file = fopen(way, "wb");
+    char *c = (char *) a;
+    for (int i = 0; i < sizeof(int) * size; i++) {
+        putc(*c++, file);
+    }
+    fclose(file);
+}
+
+
+void sortArrayInFile(char *filename, int size) {
+    int counter;
+    int a[size];
+    char *c = (char *) a;
+
+    char *way = getWayByTasks(filename);
+    FILE *file = fopen(way, "rb");
+
+    while ((counter = getc(file)) != EOF) {
+        *c = counter;
+        c++;
+    }
+    fclose(file);
+
+    int res[size];
+    int positive_index = 0;
+    int negative_index = size / 2;
+    int index_no_sort_arr = 0;
+
+    while (index_no_sort_arr < size) {
+        if (a[index_no_sort_arr] > 0)
+            res[positive_index++] = a[index_no_sort_arr++];
+        else
+            res[negative_index++] = a[index_no_sort_arr++];
+    }
+
+    generateArrayBinaryFile(filename, res, size);
+}
+
+void outputArrayFromFile(char *filename, int size) {
+    int counter;
+    int a[size];
+    char *c = (char *) a;
+
+    char *way = getWayByTasks(filename);
+    FILE *file = fopen(way, "rb");
+
+    while ((counter = getc(file)) != EOF) {
+        *c = counter;
+        c++;
+    }
+    fclose(file);
+    outputArray_(a, size);
+}
+
+void task7() {
+    int size = 10;
+    int a[size];
+    generateArray(a, size);
+    outputArray_(a, size);
+    generateArrayBinaryFile("task7.txt", a, size);
+    sortArrayInFile("task7.txt", size);
+    outputArrayFromFile("task7.txt", size);
+}
+
+void transpocseMatrixIfNotSymetricFile(char *file_name, FILE *file, int count_matrices) {
+    char *c;
+    int counter;
+    int size = sizeof(matrix) * count_matrices;
+    matrix *ptr = malloc(size);
+    file = fopen(file_name, "rb");
+    check_Correct_Open_File(file);
+    c = (char *) ptr;
+
+    while ((counter = getc(file)) != EOF) {
+        *c = counter;
+        c++;
+    }
+
+    for (int j = 0; j < count_matrices; ++j)
+        if(isSymmetricMatrix(&ptr[j]))
+            transposeMatrix(&ptr[j]);
+
+    c = (char *) ptr;
+    for (int i = 0; i < sizeof(matrix) * count_matrices; i++)
+        putc(*c++, file);
+
+    fclose(file);
+    free(ptr);
+}
+
+void task8() {
+    char *name = "task8.txt";
+    char *way = getWayByTasks(name);
+    FILE file;
+    int count_matrices = 3;
+    int values[] = {1, 2, 3,
+                    2, 2, 3,
+                    3, 3, 3,
+
+                    5, 6, 7,
+                    5, 6, 7,
+                    5, 6, 7,
+
+                    8, 9, 0,
+                    8, 9, 0,
+                    8, 9, 0 };
+
+    matrix *matrix_array = createArrayOfMatrixFromArray(values, 3, 3, 3);
+
+    WriteMatricesBinaryFile(way, &file, matrix_array,count_matrices);
+    transpocseMatrixIfNotSymetricFile(way, &file, count_matrices);
+    OutputMatricesInFile(way, &file, count_matrices);
+}
+
+typedef struct sportsman {
+    char *name;
+    int res;
+} sportsman;
+
 int main() {
    task1();
    task2();
@@ -457,5 +622,6 @@ int main() {
    task4();
    task5();
    task6();
+   task7();
 }
 
